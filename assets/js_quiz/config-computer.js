824333c11e8f82d9367aafdea1f11f8151e77b86
jlandrews <torazaemon@hotmail.com>
2022-03-11 13:24:49 +0900

    Update

---
+++ assets/js_quiz/config-computer.js
@@ -0,0 +1,451 @@
+var quizJSON = {
+    "info": {
+        "name":    "Computers and Technology",
+        "main":    "",
+        "results": "",
+        "level1":  "Excellent!",
+        "level2":  "Good!",
+        "level3":  "Average",
+        "level4":  "Bad",
+        "level5":  "Shocking!" // no comma here
+    },
+    "questions": [
+        { // Question 1
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+        { // Question 2
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": true},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+       
+        { // Question 3
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": true},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+    
+        { // Question 4
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+         { // Question 5
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+        { // Question 6
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+        { // Question 7
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": true},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+        { // Question 8
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": true},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+         { // Question 9
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+        { // Question 10
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+        { // Question 11
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": true},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+        { // Question 12
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": true},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+        { // Question 13
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+        { // Question 14
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": true},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+        { // Question 15
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": true},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+        { // Question 16
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": true},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+        { // Question 17
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+         { // Question 18
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+        { // Question 19
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": true},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+
+        { // Question 20
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": true},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },   
+
+        { // Question 21
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },   
+
+        { // Question 22
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": true},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },   
+
+        { // Question 23
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },  
+
+        { // Question 24
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": true},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+        
+        { // Question 25
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        }, 
+		
+		{ // Question 26
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": true},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+		
+		{ // Question 27
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": true},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+		
+		{ // Question 28
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+		
+		{ // Question 29
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+		
+		{ // Question 30
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": true},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+		
+		{ // Question 31
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+		
+		{ // Question 32
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": true},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+		
+		{ // Question 33
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+		{ // Question 34
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+		
+		{ // Question 35
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": true},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+		
+		{ // Question 36
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": true},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+		
+		{ // Question 37
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": true},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+		
+		{ // Question 38
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+		
+		{ // Question 39
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": false},
+                {"option": "b",    "correct": true},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+		
+		{ // Question 40
+            "q": "Choose your answer",
+            "a": [
+                {"option": "a",    "correct": true},
+                {"option": "b",    "correct": false},
+                {"option": "c",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Correct!</span></p>",
+            "incorrect": "<p><span>Fail!</span></p>" // no comma here
+        },
+    ]
+};
